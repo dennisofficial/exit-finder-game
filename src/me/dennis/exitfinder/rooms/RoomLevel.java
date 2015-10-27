d55472d74958455e57706aef6961fb18228bcd06
Dennis Lysenko <dennislysenko@hotmail.com>
2015-10-27 15:09:17 -0400

    Created CloudManager, and implemented into RoomLevel

--- src/me/dennis/exitfinder/rooms/RoomLevel.java
+++ src/me/dennis/exitfinder/rooms/RoomLevel.java
@@ -3,6 +3,7 @@ package me.dennis.exitfinder.rooms;
 import java.awt.Graphics;
 
 import me.dennis.exitfinder.enums.Level;
+import me.dennis.exitfinder.managers.CloudManager;
 import me.dennis.exitfinder.types.Background;
 import me.dennis.exitfinder.types.Room;
 import me.dennis.exitfinder.utils.MapLoader;
@@ -10,19 +11,23 @@ import me.dennis.exitfinder.utils.MapLoader;
 public class RoomLevel extends Room {
 
 	Background bg = new Background(0xACD7FF);
+	CloudManager cm = new CloudManager();
 	
 	@Override
 	public void init() {
 		objects.addAll(MapLoader.loadLevel(Level.W0L1));
+		cm.createClouds();
 	}
 
 	@Override
 	public void update() {
+		cm.update();
 	}
 
 	@Override
 	public void beginDraw(Graphics g) {
 		bg.draw(g);
+		cm.draw(g);
 	}
 
 	@Override
